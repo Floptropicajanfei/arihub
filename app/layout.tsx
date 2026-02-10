29b98dbd91a70102f8d5adcbfbac1da70c79d669
Ari-co <qbk7twb2ht@privaterelay.appleid.com>
2026-02-10 22:39:44 +0000

    V.2

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,7 +25,7 @@ export const metadata: Metadata = {
     siteName: "Ari",
     images: [
       {
-        url: "/previewv1.png",
+        url: "/previewv2.png",
         width: 1200,
         height: 630,
         alt: "Ari",
@@ -37,7 +37,7 @@ export const metadata: Metadata = {
     card: "summary_large_image",
     title: "Ari",
     description: "",
-    images: ["/previewv1.png"],
+    images: ["/previewv2.png"],
   },
 };
 
